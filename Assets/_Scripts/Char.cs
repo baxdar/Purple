bd7ee28c963cbcda121b35322ffff3870bcacd18
Galen Johnson <baxdar@gmail.com>
2017-12-23 20:34:36 -0700

    committing extremely basic char controller for posterity

---
+++ Assets/_Scripts/Char.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Char : MonoBehaviour {
+    public float movespeed = 8;
+    private Rigidbody2D charRigidBody;
+    private Vector2 movement;
+
+	// Use this for initialization
+	void Start () {
+        charRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate() {
+        charRigidBody.velocity = new Vector2(movement.x * movespeed, movement.y * movespeed);
+    }
+}
--- Assets/_Scripts/ReadMethenDelete Me.txt
+++
@@ -1,2 +0,0 @@
-should I be lazy and build a quick character controller.
-or be thorough and architect an actually intelligent input manager?
